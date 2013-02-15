7d1c9b55f1e72b3b06d989ee318a353733604efe
Nikola <nikola@jeremy-lin.(none)>
2013-02-15 14:54:51 +0100

    initial app version

---
+++ index.php
@@ -0,0 +1,149 @@
+<?php
+	header('P3P: CP="CAO PSA OUR"');
+?>
+<!doctype html>
+<!--[if IE 7]>    <html lang="en" class="no-js ie7 oldie"> <![endif]-->
+<!--[if IE 8]>    <html lang="en" class="no-js ie8 oldie"> <![endif]-->
+<!--[if gt IE 8]><!--><html lang="en" class="no-js"><!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		
+		<title>Friends Compatibility App!</title>
+		
+		<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+		<style type="text/css">
+			.centered-content {
+				text-align: center !important;
+				vertical-align: middle !important;
+			}
+			.hidden-overflow {
+				white-space: nowrap;
+				overflow: hidden;
+				text-overflow: ellipsis;
+			}
+		</style>
+	</head>
+
+	<body class="hidden-overflow">
+		<div id="fb-root"></div>
+		<script type="application/javascript">
+		// Load the SDK's source Asynchronously
+		( function(d, debug) {
+			var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+			if (d.getElementById(id)) {
+				return;
+			}
+			js = d.createElement('script');
+			js.id = id;
+			js.async = true;
+			js.src = "//connect.facebook.net/en_US/all" + ( debug ? "/debug" : "") + ".js";
+			ref.parentNode.insertBefore(js, ref);
+		}(document, /*debug*/false));
+		</script>
+		<br/>
+		<div class="container-fluid">
+			<div class="row-fluid">
+				
+				<!-- startof left column -->
+				<div id="left-div" class="span2 text-center"></div>
+				<!-- endof left column -->
+				
+				<!-- startof right column -->
+				<div class="span10">
+					<!-- startof unauth-div -->
+					<div id="unauth-div">
+						<div class="page-header">
+							<h1 id="unauth-greetings">Loading</h1>
+							<img id="loading-img" src="images/ajax-loader.gif" style="padding-left: 10px;" />
+						</div>
+						<div id="unauth-error-msgs"></div>
+					</div>
+					<!-- endof uauth-div -->
+					
+					<!-- startof auth-div -->
+					<div id="auth-div" class="invisible">
+						<div class="page-header">
+							<h1 id="auth-greetings">Greetings user!</h1>
+						</div>
+						<p>This application will analyze pages that you and your friends have liked.</p>
+						<p>It will then show you with which friends you are most compatible in a page category.</p>
+						<div id="auth-error-msgs"></div>
+						<!-- startof tabbable -->
+						<div class="tabbable">
+							<ul class="nav nav-tabs">
+								<li class="active">
+									<a href="#tab1" data-toggle="tab">Books</a>
+								</li>
+								<li>
+									<a href="#tab2" data-toggle="tab">Games</a>
+								</li>
+								<li>
+									<a href="#tab3" data-toggle="tab">Movies</a>
+								</li>
+								<li>
+									<a href="#tab4" data-toggle="tab">Music</a>
+								</li>
+							</ul>
+							<div class="tab-content">
+								<!-- startof tab1 [books] -->
+								<div class="tab-pane active hidden-overflow" id="tab1">
+									<p><a id="start-books" href="#" class="btn btn-primary btn-large calcstart">Calculate Books Compatibility »</a></p>
+									<div id="bar-books-wrap" class="progress">
+										<div id="bar-books" class="bar invisible" style="width: 0%;"></div>
+									</div>
+									<div id="placeholder-books"></div>
+								</div>
+								<!-- endof tab1 [books] -->
+								
+								<!-- startof tab2 [games] -->
+								<div class="tab-pane hidden-overflow" id="tab2">
+									<p><a id="start-games" href="#" class="btn btn-primary btn-large calcstart">Calculate Games Compatibility »</a></p>
+									<div id="bar-games-wrap" class="progress">
+										<div id="bar-games" class="bar invisible" style="width: 0%;"></div>
+									</div>
+									<div id="placeholder-games"></div>
+								</div>
+								<!-- endof tab2 [games] -->
+								
+								<!-- startof tab3 [movies] -->
+								<div class="tab-pane hidden-overflow" id="tab3">
+									<p><a id="start-movies" href="#" class="btn btn-primary btn-large calcstart">Calculate Movies Compatibility »</a></p>
+									<div id="bar-movies-wrap" class="progress">
+										<div id="bar-movies" class="bar invisible" style="width: 0%;"></div>
+									</div>
+									<div id="placeholder-movies"></div>
+								</div>
+								<!-- endof tab3 [movies] -->
+								
+								<!-- startof tab4 [music] -->
+								<div class="tab-pane hidden-overflow" id="tab4">
+									<p><a id="start-music" href="#" class="btn btn-primary btn-large calcstart">Calculate Music Compatibility »</a></p>
+									<div id="bar-music-wrap" class="progress">
+										<div id="bar-music" class="bar invisible" style="width: 0%;"></div>
+									</div>
+									<div id="placeholder-music"></div>
+								</div>
+								<!-- endof tab4 [music] -->
+							</div>
+						</div>
+						<!-- endof tabbable -->
+					</div>
+					<!-- endof auth-div -->
+				</div>
+				<!-- endof right column -->
+				
+			</div>
+		</div>
+		
+		<div id="outer-loading-div">
+			
+		</div>
+		
+		<script type="application/javascript" src="js/jquery.js"></script>
+		<script type="application/javascript" src="js/underscore.js"></script>
+		<script type="application/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+		<script type="application/javascript" src="js/main.js"></script>
+	</body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,296 @@
+// Some internal variables
+//========================
+
+var myData = myData || {
+	scope : 'user_likes, friends_likes', // Additional permissions.
+}
+var categories = categories || ['books', 'games', 'movies', 'music'];
+var err_msgs = err_msgs || [
+	{'field' : 'friends', 'msg' : "It looks like something went wrong while retrieving your list of friends."},
+	{'field' : 'games', 'msg' : "It looks like something went wrong while a list of games that you've liked. Are you sure that you've liked some Game pages recently?"}
+]
+var compatibleFriends = compatibleFriends || {}  
+
+
+// Application logic
+//==================
+
+// Fetches basic user and friend information and sets up the UI.
+function initializeApp() {
+	// Finalizes user's friends basic info after second Ajax.
+	var apiCallback2 = function(response) {
+		myData.myFriends = response.data;
+		var err_msg = _.findWhere(err_msgs, {'field' : 'friends'});
+		if(errorChecking(myData.myFriends, err_msg)) { return; } // Stop application on error.
+		// Initialize some counters for API calls later on.
+		for (var i=0; i<categories.length; i++) {
+			myData[categories[i] + 'toProcess'] = myData.myFriends.length; 
+		}
+		for (var i=0; i<myData.myFriends.length; i++) {
+			myData.myFriends[i]['mutualFaves'] = { 'books' : [], 'games' : [], 'movies' : [], 'music' : [] };
+		}
+		// Presentation & UI.
+		boot_hide('#unauth-div');
+		boot_show('#auth-div');
+		bindButtonCalculations();
+	}
+	// Finalizes basic user info after first Ajax.
+	var apiCallback1 = function(response) {
+		myData.me = response;
+		var possible = [response.first_name, response.name, response.username, response.id];
+		myData.greetingName = _(possible).chain()
+			.compact().first().value(); // Gets the first not-null string to use as greetings!
+		if(myData.greetingName) {
+			$('#auth-greetings').html("Greetings " + myData.greetingName + "!");
+		}
+		// Sets the profile picture on the left to the current user.
+		var imgSrc = getGraphImgSrc(myData.me.id) + "?type=large";
+		$('<img id="profile-image" style="display: none;" src="' + imgSrc +'" />').appendTo('#left-div');
+		$('#profile-image').toggle('slow');
+		FB.api('me/friends', apiCallback2);
+	}
+	// Gets user info from Facebook
+	FB.api('me', apiCallback1);
+}
+
+// Makes the appropriate button commence calculations if pressed.
+function bindButtonCalculations() {
+	for (var i=0; i<categories.length; i++) {
+		$('#start-' + categories[i]).on("click", { i : i }, function(event) {
+			var category = categories[event.data.i];
+			// Callback after fetching my pages from category.
+			var apiCallback1 = function(response) {
+				if(response.data) {
+					myData.me[category] = response.data;
+				} else { alert('Error fetching my data for ' + category); }
+				// UI: hide the button and show progress bar.
+				$('#start-' + category).remove();
+				$('#bar-' + category).removeClass('invisible');
+				// Now need to make a call for each friend.
+				for (var j=0; j < myData.myFriends.length; j++) {
+					fbApiFriendCategoryWrapper(j, category);
+				}
+			}
+			
+			// Start by getting user pages for category.
+			FB.api('me/' + category, apiCallback1)
+		});
+	}
+}
+
+// Wrapper for API calls for an individual friend's category pages 
+function fbApiFriendCategoryWrapper(index, category) {
+	var friend = myData.myFriends[index];
+	FB.api(friend.id + '/' + category, function(response) {
+		if (response.data) {
+			// Makes sure that we always store as Array.
+			if ($.isArray(response.data)) {
+				friend[category] = response.data;
+			} else {
+				friend[category] = [ response.data ];
+			}
+		}
+		var toProcessStr = category + "toProcess";
+		var barStr = "#bar-" + category;
+		myData[toProcessStr]--;
+		updateProgressBar(index, myData.myFriends.length, barStr);
+		if (myData[toProcessStr] == 0) {
+			// Enter calculations only when all responses were retrieved.
+			calculateSimilarities(category);
+		} else if (myData[toProcessStr] < 0) {
+			alert("Something went terribly wrong...");
+		}
+	});
+}
+
+// Returns a sorted list of friends, according to similarities in pages for given category 
+function calculateSimilarities(category) {
+	var barStr = "#bar-" + category;
+	// Extracts a simple array of ids of my pages.
+	var myPageIds = new Array();
+	for (var i=0; i < myData.me[category].length; i++) {
+		myPageIds.push(myData.me[category][i].id);
+		var barStr = "#bar-" + category;
+		updateProgressBar(i, myData.me[category].length, barStr);
+	}
+	// For each friend, go through their list of pages (if any) a remove pages not on my list.
+	for (var i=0; i < myData.myFriends.length; i++) {
+		// i-th friend
+		//myData.myFriends[i].mutualFaves[category] = []
+		if (myData.myFriends[i][category].length > 0) {
+			var friendCategoryList = myData.myFriends[i][category];
+			for (var j=0; j < friendCategoryList.length; j++) {
+				// Their j-th page
+				var friendPageId = friendCategoryList[j].id;
+				if (myPageIds.indexOf(friendPageId) >= 0) {
+					// Their page is on our list of favourites.
+					myData.myFriends[i].mutualFaves[category].push(friendPageId);
+				}
+			}
+		}
+		updateProgressBar(i, myData.myFriends.length, barStr);
+	}
+	// Create a new array, sorted by the number of mutual favourites in given category
+	compatibleFriends[category] = _.clone(myData.myFriends).sort(function(u1, u2){
+		if (u1.mutualFaves[category].length < u2.mutualFaves[category].length) return 1;
+		if (u1.mutualFaves[category].length > u2.mutualFaves[category].length) return -1;
+		return 0
+	});
+	$(barStr + "-wrap").remove(); // Removes the progress bar for this category.
+	displayResults(category); // Calls the function which presents compatible friends for this page category.
+}
+
+function displayResults(category) {
+	placeholder = $('#placeholder-' + category);
+	// Make sure that at least one friend has some pages in common.
+	var nFriends = compatibleFriends[category].length;
+	var tableId = "table-" + category; // Id for DOM element. 
+	if (compatibleFriends[category][0].mutualFaves[category].length > 0) {
+		$('<p class="lead">The following friends share your taste in ' + category
+			+ ':</p><table id="' + tableId +'" class="table" style="width: 100%;"></table>').insertBefore(placeholder);
+		// Generates a table row for each friend and prepends it to the previously created table.
+		var table = $('#' + tableId);
+		for (var i=0; i < nFriends; i++) {
+			var friend = compatibleFriends[category][i];
+			if (friend.mutualFaves[category].length > 0) {
+				var row = "<tr>"; // Resulting html for the row
+				var nFaves = friend.mutualFaves[category].length;
+				var profileUrl = "https://www.facebook.com/" + friend.id;
+				var _page = nFaves > 1 ? " pages" : " page";
+				var sampleCategoryFaves = _.chain(friend.mutualFaves[category]).shuffle().first(10).value();
+				var thumbnails = ""; // Page thumbnails, generate up to 8 maximum
+				for (var j=0; j < _.min([sampleCategoryFaves.length, 8]); j++) {
+					thumbnails +='<a target="_blank" href="http://www.facebook.com/' + sampleCategoryFaves[j]
+						+'"><img src="' + getGraphImgSrc(sampleCategoryFaves[j])+ '?type=square" /></a>';
+				}
+				// Shows "..." if more than 8 items present.
+				if(sampleCategoryFaves.length > 8) { thumbnails += "<img src='images/ellipsis20.png'/>"; }
+				// 1. column - profile picture, linked
+				row += '<td class="centered-content"><a href="' + profileUrl +'"><img src="' + getGraphImgSrc(friend.id)+ '?width=65&height=65" class="img-polaroid"/></a></td>';
+				// 2. column - name, number of pages in common, some thumnails
+				row += '<td><a href="' + profileUrl + '"><h5>' + friend.name +'</h5></a>'
+					+ '<p>You both love ' + nFaves + _page + " from the " + category + " category.</p>"
+					+ '<p style=" ">' + thumbnails + '</p></td>';
+				// End of row.
+				row += "</row>"
+				table.append(row);
+			} else {
+				i = compatibleFriends[category].length; // Found the first friend with no mutually liked pages.
+			}
+		}
+		FB.Canvas.setAutoGrow(); // Makes sure the Canvas is resized.
+	} else {
+		var noMutualMessage = "Hmm, it looks like your friends don't really share your taste in " + category + "...";
+		$('<div class="alert">').html(noMutualMessage).insertBefore(placeholder);
+	}
+}
+
+
+// Facebook & Application Initialization Code.
+//==============================
+
+// Code called on DOM 
+$(document).ready(function() {
+	if(window.FB) {
+		init();
+	} else {
+		window.fbAsyncInit = init;
+	}
+});
+
+function init() {
+	FB.init({
+		appId : '328988547203285',
+		channelUrl : "vast-shelf-9526.herokuapp.com/channel.html",
+		status : true,
+		cookie : true,
+		oauth : true,
+		xfbml : true
+	});
+	
+	$(document).trigger("facebook:ready");
+}
+
+// Code called on "facebook:ready" event
+$(document).on("facebook:ready", function() {
+	authorize();
+});
+
+/**
+ * Checks whether user login status and prompts for permissions.
+ * Upon success, calls function to initialize the application. 
+ */
+function authorize() {
+	FB.getLoginStatus(function(response) {
+		if (response.status == 'connected') {
+			// User is already connected with the App.
+			handleUserConnect(response);
+		} else {
+			// Either not connected to app or not logged in to Facebook.
+			login();
+		}
+	}, true); // Second parameter forces a roundtrip to Facebook - effectively refreshing the cache of the response object.
+}
+
+// Wrapper around FB.login
+function login() {
+    FB.login(function(response) {
+    	console.log(response);
+        if (response.authResponse) {
+            // Connected
+            handleUserConnect(response);
+        } else {
+            // Cancelled
+            handleUserCancel();
+        }
+    }, { scope: myData.scope }); 
+}
+
+function handleUserConnect(response) {
+	myData.authResponse = response.authResponse;
+	initializeApp();
+}
+
+function handleUserCancel() {
+	$('<img id="profile-image" src="images/unknown-user.gif" style="display: none"/>').appendTo("#left-div");
+	$('#profile-image').toggle('slow');
+	$('#loading-img').toggle('slow');
+	$("<div class='alert'>Oh well, I guess you don't want to find out which friends are most <i>like</i> you...</div>").appendTo('#unauth-error-msgs');
+}
+
+
+// Utility Functions
+//==================
+
+// Returns a Picture of an entity from Facebook Graph via its id.
+function getGraphImgSrc(id) {
+	return "https://graph.facebook.com/" + id +"/picture";
+}
+
+// Inserts error alert upon empty field.
+function errorChecking(field, errorMessage, containerStr) {
+	if(!field || field.length == 0 ) {
+		// Insert error message div and return True.
+		$('<div class="alert alert-error">').html(errorMessage).appendTo(containerStr);
+		return true;
+	} else { return false; }
+}
+
+// A function to update the CSS progress bar on page.
+function updateProgressBar(numerator, denominator, progressBarSelectorStr) {
+	var numerator = parseInt(numerator);
+	var denominator = parseInt(denominator);
+	var percentage = "0%";
+	if (denominator != 0) {
+		percentage = (100 * numerator / denominator).toString() + "%";
+	}
+	$(/*'#friend-progress'*/progressBarSelectorStr).css('width', percentage);
+}
+
+function boot_hide(select_str) {
+	return $(select_str).addClass('invisible').hide('slow');
+}
+
+function boot_show(select_str) {
+	return $(select_str).removeClass('invisible').show('slow');
+}
